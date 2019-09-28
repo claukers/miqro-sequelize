0c7e6ea95c7baef6bf0cc00a61bb3ae40f349eaf
claukers <claukers@melia>
2019-09-28 07:12:23 -0300

    loader autogenerates if .sequelizerc doesnt exists!

--- src/util/loader.ts
+++ src/util/loader.ts
@@ -1,9 +1,42 @@
 "use strict";
+import * as  fs from "fs";
 import { Util } from "miqro-core";
 import * as  path from "path";
+import {templates} from "./template";
 
 export const setupDB = () => {
   Util.checkEnvVariables(["MIQRO_DIRNAME"]);
-  const sequelizerc = require(path.resolve(process.env.MIQRO_DIRNAME, ".sequelizerc"));
+  const sequelizercPath = path.resolve(process.env.MIQRO_DIRNAME, ".sequelizerc");
+  if (!fs.existsSync(sequelizercPath)) {
+    const configFolder = path.resolve(process.env.MIQRO_DIRNAME, "config");
+    const dbFolder = path.resolve(process.env.MIQRO_DIRNAME, "db");
+    const migrationsFolder = path.resolve(dbFolder, "migrations");
+    const modelsFolder = path.resolve(dbFolder, "models");
+    const seedersFolder = path.resolve(dbFolder, "seeders");
+    const modelLoaderPath = path.resolve(modelsFolder, "index.js");
+    const dbConfigFilePath = path.resolve(configFolder, "db.js");
+    if (!fs.existsSync(dbFolder)) {
+      fs.mkdirSync(dbFolder);
+    }
+    if (!fs.existsSync(dbConfigFilePath)) {
+      fs.writeFileSync(dbConfigFilePath, templates.dbConfig);
+    }
+    if (!fs.existsSync(sequelizercPath)) {
+      fs.writeFileSync(sequelizercPath, templates.sequelizerc);
+    }
+    if (!fs.existsSync(migrationsFolder)) {
+      fs.mkdirSync(migrationsFolder);
+    }
+    if (!fs.existsSync(modelsFolder)) {
+      fs.mkdirSync(modelsFolder);
+    }
+    if (!fs.existsSync(modelLoaderPath)) {
+      fs.writeFileSync(modelLoaderPath, templates.modelsIndex);
+    }
+    if (!fs.existsSync(seedersFolder)) {
+      fs.mkdirSync(seedersFolder);
+    }
+  }
+  const sequelizerc = require(sequelizercPath);
   return require(sequelizerc["models-path"]);
 };
---
+++ src/util/template.ts
@@ -0,0 +1,78 @@
+const modelsIndex =
+  `'use strict';
+
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const sequelizerc = require(path.resolve(__dirname, '..', '..', '.sequelizerc'));
+const config = require(sequelizerc.config);
+
+const modelsPath = __dirname;
+
+
+const db = {
+  sequelize: new Sequelize(config.database, config.username, config.password, config),
+  Sequelize
+};
+
+fs
+  .readdirSync(modelsPath)
+  .filter((file) => {
+    return (file !== "index.js") && (file.indexOf(".") !== 0) && (file.slice(-3) === ".js");
+  })
+  .forEach((file) => {
+    const model = db.sequelize.import(path.join(modelsPath, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+module.exports = db;
+`;
+const dbConfig =
+  `["DB_DIALECT_SSL", "DB_PORT", "DB_NAME", "DB_USER", "DB_PASS", "DB_HOST", ` +
+  `"DB_DIALECT", "DB_POOL_MAX", "DB_POOL_MIN", "DB_POOL_ACQUIRE", "DB_POOL_IDDLE", "DB_STORAGE"].forEach((envName) => {
+if (process.env[envName] === undefined) {
+  throw new Error(\`Env variable [\${envName}!] not defined\`);
+}
+});
+
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: process.env.DB_DIALECT_SSL === "true"
+  },
+  pool: {
+    acquire: parseInt(process.env.DB_POOL_ACQUIRE, 10),
+    idle: parseInt(process.env.DB_POOL_IDDLE, 10),
+    max: parseInt(process.env.DB_POOL_MAX, 10),
+    min: parseInt(process.env.DB_POOL_MIN, 10)
+  },
+  storage: process.env.DB_STORAGE
+};
+`;
+
+const sequelizerc =
+  `const path = require("path");
+
+module.exports = {
+  "config": path.resolve(__dirname, "config", "db.js"),
+  "migrations-path": path.resolve(__dirname, "db", "migrations"),
+  "seeders-path": path.resolve(__dirname, "db", "seeders"),
+  "models-path": path.resolve(__dirname, "db", "models"),
+};
+`;
+
+export const templates = {
+  modelsIndex,
+  dbConfig,
+  sequelizerc
+};
