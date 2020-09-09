7d2675df2d816c1dc5faefcbf420cd60c8db8dc4
claukers <claukers@amelia>
2020-09-09 04:25:48 -0300

    fixlint

--- src/cli/migration-status.ts
+++ src/cli/migration-status.ts
@@ -1,5 +1,5 @@
 import {Util} from "@miqro/core";
-import {migrateStatus, undoSeed} from "../db";
+import {migrateStatus} from "../db";
 
 if (process.argv.length !== 3) {
   throw new Error(`usage: miqro-database seed`);
