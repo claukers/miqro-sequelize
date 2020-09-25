83e9d3587601bddc1b09c0f38142bbcaab921fa3
claukers <claukers@amelia>
2020-09-25 04:51:25 -0300

    fix typos

--- src/service/deleted.ts
+++ src/service/deleted.ts
@@ -1,35 +1,36 @@
 import {PostListModelService} from "./postlist";
 import {ModelServiceArgs} from "./model";
+import {Transaction} from "sequelize";
 
 export class FakeDeleteModelService extends PostListModelService {
   protected modelIsDeletedAttribute = "deleted";
 
-  public async get(args: ModelServiceArgs): Promise<any> {
+  public async get(args: ModelServiceArgs, transaction?: Transaction, skipLocked?: boolean): Promise<any> {
     args.params[this.modelIsDeletedAttribute] = false;
-    return super.get(args);
+    return super.get(args, transaction, skipLocked);
   }
 
-  public async delete(args: ModelServiceArgs): Promise<any> {
+  public async delete(args: ModelServiceArgs, transaction?: Transaction): Promise<any> {
     args.body[this.modelIsDeletedAttribute] = true;
-    return super.patch(args);
+    return super.patch(args, transaction);
   }
 
-  public async patch(args: ModelServiceArgs): Promise<any> {
+  public async patch(args: ModelServiceArgs, transaction?: Transaction): Promise<any> {
     if (args.body[this.modelIsDeletedAttribute]) {
       delete args.body[this.modelIsDeletedAttribute];
     }
-    return super.patch(args);
+    return super.patch(args, transaction);
   }
 
-  public async post(args: ModelServiceArgs): Promise<any> {
+  public async post(args: ModelServiceArgs, transaction?: Transaction): Promise<any> {
     if (args.body instanceof Array) {
       for (const rBody of args.body) {
         rBody[this.modelIsDeletedAttribute] = false;
       }
-      return super.post(args);
+      return super.post(args, transaction);
     } else {
       args.body[this.modelIsDeletedAttribute] = false;
-      return super.post(args);
+      return super.post(args, transaction);
     }
   }
 }
--- src/service/postlist.ts
+++ src/service/postlist.ts
@@ -1,11 +1,12 @@
 import {ModelService} from "./smodel";
 import {ModelServiceArgs} from "./model";
 import {Util} from "@miqro/core";
+import {Transaction} from "sequelize";
 
 export class PostListModelService extends ModelService {
 
   /* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
-  public async post(args: ModelServiceArgs, transaction?: any): Promise<any> {
+  public async post(args: ModelServiceArgs, transaction?: Transaction): Promise<any> {
     if (args.body instanceof Array) {
       Util.parseOptions("params", args.params, [], "no_extra");
       Util.parseOptions("query", args.query, [], "no_extra");
