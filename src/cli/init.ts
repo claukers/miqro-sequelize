205b3aa5d3aaf1a69ea7ce53c5b84ba880abc924
claukers <claukers@localhost>
2021-01-29 01:05:17 -0300

    cli moved to miqro module

--- src/cli/init.ts
+++
@@ -1,9 +0,0 @@
-import {initDBConfig} from "../db";
-
-export const main = (): void => {
-  if (process.argv.length !== 3) {
-    throw new Error(`invalid number of args`);
-  }
-
-  initDBConfig();
-};
